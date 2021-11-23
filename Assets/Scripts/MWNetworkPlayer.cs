4ade06efab9df2e93dc493b30a8e05c421e9a6a8
Luis Carlos B <luboteroa@unal.edu.co>
2021-11-23 01:42:50 -0500

    Server authority, sync variables

--- Assets/Scripts/MWNetworkPlayer.cs
+++ Assets/Scripts/MWNetworkPlayer.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using Mirror;
 
 public class MWNetworkPlayer : NetworkBehaviour
 {
-    [SyncVar]
+    [SerializeField] private TMP_Text displayNameText;
+    [SerializeField] private Renderer displayColourRenderer;
+
+    [SyncVar(hook = nameof(HandleDisplayNameUpdated))]
     [SerializeField]
     private string displayName = "Missing name";
 
-    [SyncVar]
+    [SyncVar(hook = nameof(HandleDisplayColourUpdated))]
     [SerializeField]
     private Color displayColor = Color.black;
 
+    #region Server
     [Server]
     public void SetDisplayName(string newDisplayName)
     {
@@ -24,4 +29,41 @@ public class MWNetworkPlayer : NetworkBehaviour
     {
         displayColor = newDisplayColor;
     }
+
+    [Command]
+    private void CmdSetDisplayName(string newDisplayName)
+    {
+        if (newDisplayName.Length < 2 || newDisplayName.Length > 20){return;}
+        RpcLogNewName(newDisplayName);
+
+        SetDisplayName(newDisplayName);
+    }
+
+    #endregion
+
+    #region  Client
+
+    private void HandleDisplayColourUpdated(Color oldColour, Color newColour)
+    {
+        displayColourRenderer.material.SetColor("_BaseColor", newColour);
+    }
+
+    private void HandleDisplayNameUpdated(string oldName, string newName)
+    {
+        displayNameText.text = newName;    
+    }
+
+    [ContextMenu("Set name")]
+    private void SetMyName()
+    {
+        CmdSetDisplayName("My New Name");
+    }
+
+    [ClientRpc]
+    private void RpcLogNewName(string newDisplayName)
+    {
+        Debug.Log(newDisplayName);
+    }
+
+    #endregion
 }
