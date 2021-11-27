ada17490e3cc17a545e15c5d2c189a483f8840ae
Luis Carlos B <luboteroa@unal.edu.co>
2021-11-27 12:22:09 -0500

    Network transform and Player Movement autorithy

--- Assets/Scripts/MWNetworkManager.cs
+++ Assets/Scripts/MWNetworkManager.cs
@@ -8,8 +8,6 @@ public class MWNetworkManager : NetworkManager
     public override void OnClientConnect(NetworkConnection conn)
     {
         base.OnClientConnect(conn);
-
-        Debug.Log("Connecter to server!");
     }
 
     public override void OnServerAddPlayer(NetworkConnection conn)
@@ -26,6 +24,5 @@ public class MWNetworkManager : NetworkManager
             Random.Range(0f, 1f));
 
         player.SetDisplayColor(displayColor);
-        Debug.Log($"There are now {numPlayers} players");
     }
 }
