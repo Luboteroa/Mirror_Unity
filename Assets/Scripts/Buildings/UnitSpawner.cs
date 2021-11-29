0e45b8e053c068977cbc84b161a4d211b3c2a928
Luboteroa <64756525+Luboteroa@users.noreply.github.com>
2021-11-29 18:54:13 -0500

    Prefabs spawn

---
+++ Assets/Scripts/Buildings/UnitSpawner.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using Mirror;
+using UnityEngine.EventSystems;
+
+public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
+{
+    [SerializeField] private GameObject unitPrefab;
+    [SerializeField] private Transform unitSpawnPoint;
+
+    #region Server
+
+    [Command]
+    private void CmdSpawnUnit()
+    {
+        GameObject unitInstance = Instantiate(
+            unitPrefab,
+            unitSpawnPoint.position, 
+            unitSpawnPoint.rotation);
+
+        NetworkServer.Spawn(unitInstance, connectionToClient);
+    }
+
+    #endregion
+
+    #region  Client
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.button != PointerEventData.InputButton.Left) {return;}
+
+        if(!hasAuthority){return;}
+
+        CmdSpawnUnit();
+    }
+
+    #endregion
+}
